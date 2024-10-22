0a5ee77212e2b6461836e5bcea340361050dfc71
Tushar-2702 <tushar2004@gmail.com>
2024-10-22 19:42:49 +0530

    commit

---
+++ src/main/java/com/chatapp/Main.java
@@ -0,0 +1,11 @@
+package com.chatapp;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        App app = new App();
+        Application.launch(App.class,args);
+    }
+}
